49e48a5fa872a5a2550a6c407385b13840324e4f
killianL <klecorf@normandiewebschool.fr>
2023-12-02 00:20:48 +0100

    end carrousel

--- lecerfdesign/src/Components/SkillCarrouselle/SkillCarrousselle.tsx
+++ lecerfdesign/src/Components/SkillCarrouselle/SkillCarrousselle.tsx
@@ -2,75 +2,106 @@
 import React, { useState } from 'react';
 import './_SkillCarrousselle.scss';
 import SkillCard from '../SkillCard/SkillCard';
-import skillImageLogo from "../../../public/Assets/Img/skillLogo.svg";
-import logoLogo from "../../../public/Assets/Img/logoLogo.svg";
-import skillImageWebDesign from "../../../public/Assets/Img/skillwebdesign.svg";
-import logoWebDesign from "../../../public/Assets/Img/logowebdesign.svg";
+import skillImageLogo from '../../../public/Assets/Img/skillLogo.svg';
+import logoLogo from '../../../public/Assets/Img/logoLogo.svg';
+import skillImageWebDesign from '../../../public/Assets/Img/skillwebdesign.svg';
+import logoWebDesign from '../../../public/Assets/Img/logowebdesign.svg';
+import logoImprimes from '../../../public/Assets/Img/logoImprimes.svg';
+import skillImprimes from '../../../public/Assets/Img/skillImprimes.svg';
+import skillVideo from '../../../public/Assets/Img/skillVideo.svg';
+import logoVideo from '../../../public/Assets/Img/logoVideo.svg';
+import { SlArrowRight, SlArrowLeft } from 'react-icons/sl';
 
 const SkillCarrousselle: React.FC = () => {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
 
   const items = [
-    {
-      SkillImage: skillImageLogo,
-      SkillLogo: logoLogo,
-      SkillDescription: 'Skill 1 Description',
-    },
-    {
-      SkillImage: skillImageWebDesign,
-      SkillLogo: logoWebDesign,
-      SkillDescription: 'Skill 2 Description',
-    },
-    {
-      SkillImage: skillImageWebDesign,
-      SkillLogo: logoWebDesign,
-      SkillDescription: 'Skill 3 Description',
-    },
-    {
-      SkillImage: skillImageWebDesign,
-      SkillLogo: logoWebDesign,
-      SkillDescription: 'Skill 4 Description',
-    }
+    [
+      {
+        SkillImage: skillImageLogo,
+        SkillLogo: logoLogo,
+        SkillDescription: 'Skill 1 Description',
+      },
+      {
+        SkillImage: skillImageWebDesign,
+        SkillLogo: logoWebDesign,
+        SkillDescription: 'Skill 2 Description',
+      },
+    ],
+    [
+      {
+        SkillImage: skillImageWebDesign,
+        SkillLogo: logoWebDesign,
+        SkillDescription: 'Skill 2 Description',
+      },
+      {
+        SkillImage: skillImprimes,
+        SkillLogo: logoImprimes,
+        SkillDescription: 'Skill 3 Description',
+      },
+    ],
+    [
+      {
+        SkillImage: skillImprimes,
+        SkillLogo: logoImprimes,
+        SkillDescription: 'Skill 3 Description',
+      },
+      {
+        SkillImage: skillVideo,
+        SkillLogo: logoVideo,
+        SkillDescription: 'Skill 4 Description',
+      },
+    ],
+    [
+      {
+        SkillImage: skillVideo,
+        SkillLogo: logoVideo,
+        SkillDescription: 'Skill 4 Description',
+      },
+      {
+        SkillImage: skillImageLogo,
+        SkillLogo: logoLogo,
+        SkillDescription: 'Skill 1 Description',
+      },
+    ],
   ];
 
-  const slidesToShow = 2;
-
-  const totalSlides = Math.ceil(items.length / slidesToShow);
+  const totalSlides = items.length;
 
   const nextSlide = (): void => {
     setCurrentSlide((prevSlide) => (prevSlide === totalSlides - 1 ? 0 : prevSlide + 1));
   };
-  
+
   const prevSlide = (): void => {
     setCurrentSlide((prevSlide) => (prevSlide === 0 ? totalSlides - 1 : prevSlide - 1));
   };
 
-  const startIndex = currentSlide * slidesToShow;
-  const endIndex = startIndex + slidesToShow;
+  const startIndex = currentSlide % totalSlides;
 
   return (
     <div className="carousel">
       <div className="slider">
-        {items.slice(startIndex, endIndex).map((item, i) => (
-          <SkillCard
-            key={i}
-            SkillImage={item?.SkillImage || ''}
-            SkillLogo={item?.SkillLogo || ''}
-            SkillDescription={item?.SkillDescription || ''}
-          />
+        <button onClick={prevSlide} className="prevButton">
+          <SlArrowLeft />
+        </button>
+        {items[startIndex].map((itemPair, i) => (
+          <div className="skillPair" key={i}>
+            <SkillCard
+              SkillImage={itemPair.SkillImage}
+              SkillLogo={itemPair.SkillLogo}
+              SkillDescription={itemPair.SkillDescription}
+            />
+          </div>
         ))}
+        <button onClick={nextSlide} className="nextButton">
+          <SlArrowRight />
+        </button>
       </div>
-      <button onClick={prevSlide} className="prevButton">
-        Previous
-      </button>
-      <button onClick={nextSlide} className="nextButton">
-        Next
-      </button>
       <div className="indicators">
         {[...Array(totalSlides)].map((_, index) => (
           <span
             key={index}
-            className={`indicator ${index === currentSlide ? 'activeIndicator' : ''}`}
+            className={`indicator ${index === startIndex ? 'activeIndicator' : ''}`}
             onClick={() => setCurrentSlide(index)}
           />
         ))}
