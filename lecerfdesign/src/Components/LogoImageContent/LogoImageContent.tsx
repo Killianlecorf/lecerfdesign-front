ad5c76a095fb43f18391b02d5fa6413fa1085bce
killian-lecorf <killianlecorf@outlook.fr>
2023-12-07 10:49:41 +0100

    end logo page

--- lecerfdesign/src/Components/LogoImageContent/LogoImageContent.tsx
+++ lecerfdesign/src/Components/LogoImageContent/LogoImageContent.tsx
@@ -6,12 +6,22 @@ const LogoImageContent = () => {
     return (
         <div className='LogoImageContent'>
             <div className="imageContent">
-                <Image src="/assets/img/logo_1.svg" alt='logo Noble Site' width={275} height={307} />
-                <Image src="/assets/img/logo_4.svg" alt='Logo O&apos;ZOO' width={277} height={336}  />
-                <Image src="/assets/img/logo_2.svg" alt='Logo O&apos;ZOO' width={279} height={279}  />
-                <Image src="/assets/img/logo_5.svg" alt='Logo O&apos;ZOO' width={277} height={438}  />
-                <Image src="/assets/img/logo_3.svg" alt='Logo O&apos;ZOO' width={277} height={440}  />
-                <Image src="/assets/img/logo_6.svg" alt='Logo O&apos;ZOO' width={277} height={440}  />
+                <div className="logoContent1">
+                    <Image src="/assets/img/logo_1.svg" alt='logo Noble Site' width={277} height={307} />
+                    <Image src="/assets/img/logo_4.svg" alt='Logo O&apos;ZOO' width={277} height={336}  />
+                    <Image src="/assets/img/logo_7.svg" alt='Logo O&apos;ZOO' width={277} height={336}  />
+                    <Image src="/assets/img/logo_10.svg" alt='Logo O&apos;ZOO' width={277} height={336}  />
+                </div>
+                <div className="logoContent2">
+                    <Image src="/assets/img/logo_2.svg" alt='logo Noble Site' width={277} height={279} />
+                    <Image src="/assets/img/logo_5.svg" alt='Logo O&apos;ZOO' width={277} height={438}  />
+                    <Image src="/assets/img/logo_8.svg" alt='Logo O&apos;ZOO' width={277} height={279}  />
+                </div>
+                <div className="logoContent3">
+                    <Image src="/assets/img/logo_3.svg" alt='logo Noble Site' width={277} height={440} />
+                    <Image src="/assets/img/logo_6.svg" alt='Logo O&apos;ZOO' width={277} height={336}  />
+                    <Image src="/assets/img/logo_9.svg" alt='Logo O&apos;ZOO' width={277} height={336}  />
+                </div>
             </div>
         </div>
     );
