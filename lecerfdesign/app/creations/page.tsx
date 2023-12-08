c45cb986f10f94ae1a76f9eac99921f98c87172d
killian-lecorf <killianlecorf@outlook.fr>
2023-12-08 11:57:26 +0100

    add all page

---
+++ lecerfdesign/app/creations/page.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/Components/Footer/Footer';
+import Header from '@/Components/Header/Header';
+import React from 'react';
+
+const Creations = () => {
+    return (
+        <div>
+            <Header currentPath="/creations"/>
+            <Footer />
+        </div>
+    );
+};
+
+export default Creations;
---
+++ lecerfdesign/app/printed/page.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/Components/Footer/Footer';
+import Header from '@/Components/Header/Header';
+import React from 'react';
+
+const Printed = () => {
+    return (
+        <div>
+            <Header currentPath="/printed"/>
+            <Footer />
+        </div>
+    );
+};
+
+export default Printed;
