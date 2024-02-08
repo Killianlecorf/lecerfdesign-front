a39d0adbda2ed4019ca2838f025acc6129f19e87
killian-lecorf <killianlecorf@outlook.fr>
2024-02-08 14:18:07 +0100

    end hover logo card

---
+++ lecerfdesign/src/Components/LogoImageContent/CardLogo/CardLogo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface CardLogoProps {
+    LinkImage: string; 
+    WidthImage: number;
+    HeightImage: number;
+    Title: string;
+    Content: string;
+    AltText: string;
+}
+
+const CardLogo: React.FC<CardLogoProps> = ({ LinkImage, WidthImage, HeightImage, Title, Content, AltText }) => {
+    return (
+        <div className='card'>
+            <img src={LinkImage} width={WidthImage} height={HeightImage} alt={AltText} />
+            <div className="hoverImage">
+                <div className="sizeCard">
+                    <h2>{Title}</h2>
+                    <p>{Content}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CardLogo;
--- lecerfdesign/src/Components/LogoImageContent/LogoImageContent.tsx
+++ lecerfdesign/src/Components/LogoImageContent/LogoImageContent.tsx
@@ -1,51 +1,110 @@
 import Image from 'next/image';
 import React from 'react';
 import './_LogoImageContent.scss';
+import CardLogo from '@/Components/LogoImageContent/CardLogo/CardLogo';
 
 const LogoImageContent = () => {
     return (
         <div className='LogoImageContent'>
             <div className="imageContent">
                 <div className="logoContent1">
-                    <div className='card'>
-                        <Image src="/Assets/Img/logo_1.jpg" width={277} height={307} alt='logo Noble Site' />
-                        <div className="hoverImage">
-                            <div className="sizeCard">
-                                <h2>Noble site</h2>
-                                <p>Création d&apos;un logo pour une entreprise de marketing digitale Noble Site </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <Image src="/Assets/Img/logo_4.jpg" width={277} height={336} alt='Mock-up'/>
-                        <div className="hoverImage">
-                        </div>
-                    </div>
-                    <Image src="/Assets/Img/logo_7.jpg" width={277} height={336} alt='Mock-up'/>
-                    <Image src="/Assets/Img/logo_10.jpg" width={277} height={336} alt='Mock-up' />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_1.jpg' 
+                        WidthImage={277} 
+                        HeightImage={307} 
+                        Title='Noble site'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise Noble Site'
+                        AltText='logo Noble Site' 
+                    />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_4.jpg' 
+                        WidthImage={277} 
+                        HeightImage={336} 
+                        Title='SEIF DEIT'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise SEIF DEIT '
+                        AltText='logo SEIF DEIT' 
+                    />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_7.jpg' 
+                        WidthImage={277} 
+                        HeightImage={336} 
+                        Title='100% CAFÉ'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise 100% Café'
+                        AltText='logo 100% CAFE' 
+                    />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_10.jpg' 
+                        WidthImage={277} 
+                        HeightImage={336} 
+                        Title='TutoDuel Académie'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise TutoDuel Académie'
+                        AltText='logo TutoDuel Académie' 
+                    />
                 </div>
                 <div className="logoContent2">
-                    <Image src="/Assets/Img/logo_2.jpg" width={277} height={279} alt='Logo O&apos;ZOO' />
-                    <Image src="/Assets/Img/logo_5.jpg" width={277} height={438} alt='Mock-up' />
-                    <Image src="/Assets/Img/logo_8.jpg" width={277} height={279} alt='Mock-up' />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_2.jpg' 
+                        WidthImage={277} 
+                        HeightImage={279} 
+                        Title='TutoDuel Académie'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise TutoDuel Académie'
+                        AltText='logo TutoDuel Académie' 
+                    />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_5.jpg' 
+                        WidthImage={277} 
+                        HeightImage={438} 
+                        Title='LECERF DESIGN'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise LECERF Design'
+                        AltText='logo LECERF Design' 
+                    />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_8.jpg' 
+                        WidthImage={277} 
+                        HeightImage={279} 
+                        Title='Kross_Officiel'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise Kross_Officiel'
+                        AltText='logo Kross_Officiel' 
+                    />
                 </div>
                 <div className="logoContent3">
-                    <Image src="/Assets/Img/logo_3.jpg" width={277} height={440} alt='Mock-up' />
-                    <Image src="/Assets/Img/logo_6.jpg" width={277} height={336} alt='Mock-up' />
-                    <Image src="/Assets/Img/logo_9.jpg" width={277} height={336} alt='Mock-up'/>
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_3.jpg' 
+                        WidthImage={277} 
+                        HeightImage={440} 
+                        Title='MUSCLE SHEEP'
+                        Content='Création de l&apos;identité visuelle Musle Sheep'
+                        AltText='logo Musle Sheep' 
+                    />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_6.jpg' 
+                        WidthImage={277} 
+                        HeightImage={336} 
+                        Title='Sky Limit'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise Sky Limit'
+                        AltText='logo Sky Limit' 
+                    />
+                    <CardLogo 
+                        LinkImage='/Assets/Img/logo_9.jpg' 
+                        WidthImage={277} 
+                        HeightImage={336} 
+                        Title='HANKALA'
+                        Content='Création de l&apos;identité visuelle de l&apos;entreprise Hankala'
+                        AltText='logo Sky Limit' 
+                    />
                 </div>
             </div>
             <div className="imageContentResponsive">
-                    <Image src="/Assets/Img/logo_1_responsive.jpg" width={240} height={114} alt='logo Noble Site' />
-                    <Image src="/Assets/Img/logo_2_responsive.jpg" width={240} height={114} alt='Mock-up'/>
-                    <Image src="/Assets/Img/logo_3_responsive.jpg" width={240} height={114} alt='Mock-up'/>
-                    <Image src="/Assets/Img/logo_4_responsive.jpg" width={240} height={114} alt='Mock-up' />
-                    <Image src="/Assets/Img/logo_5_responsive.jpg" width={240} height={114} alt='logo Noble Site' />
-                    <Image src="/Assets/Img/logo_6_responsive.jpg" width={240} height={114} alt='Mock-up'/>
-                    <Image src="/Assets/Img/logo_7_responsive.jpg" width={240} height={114} alt='Mock-up'/>
-                    <Image src="/Assets/Img/logo_8_responsive.jpg" width={240} height={114} alt='Mock-up' />
-                    <Image src="/Assets/Img/logo_9_responsive.jpg" width={240} height={114} alt='logo Noble Site' />
-                    <Image src="/Assets/Img/logo_10_responsive.jpg" width={240} height={114} alt='Mock-up'/>
+                <Image src="/Assets/Img/logo_1_responsive.jpg" width={240} height={114} alt='logo Noble Site' />
+                <Image src="/Assets/Img/logo_2_responsive.jpg" width={240} height={114} alt='Mock-up'/>
+                <Image src="/Assets/Img/logo_3_responsive.jpg" width={240} height={114} alt='Mock-up'/>
+                <Image src="/Assets/Img/logo_4_responsive.jpg" width={240} height={114} alt='Mock-up' />
+                <Image src="/Assets/Img/logo_5_responsive.jpg" width={240} height={114} alt='logo Noble Site' />
+                <Image src="/Assets/Img/logo_6_responsive.jpg" width={240} height={114} alt='Mock-up'/>
+                <Image src="/Assets/Img/logo_7_responsive.jpg" width={240} height={114} alt='Mock-up'/>
+                <Image src="/Assets/Img/logo_8_responsive.jpg" width={240} height={114} alt='Mock-up' />
+                <Image src="/Assets/Img/logo_9_responsive.jpg" width={240} height={114} alt='logo Noble Site' />
+                <Image src="/Assets/Img/logo_10_responsive.jpg" width={240} height={114} alt='Mock-up'/>
             </div>
         </div>
     );
