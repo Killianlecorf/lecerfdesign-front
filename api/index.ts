37a5c9f802debb2ac9afb79885910fb1e29d118c
killian-lecorf <Klecorf@normandiewebschool.fr>
2024-03-08 22:51:11 +0100

    edit cors on server file

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ 
+    origin: 'http://vps-3aa18acd.vps.ovh.net:3000' 
+}))
 
 app.use(bodyParser.json());
 
