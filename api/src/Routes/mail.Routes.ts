6c0dbee233e337b61fe02a834e5033d3ef7cc55b
killian-lecorf <Klecorf@normandiewebschool.fr>
2024-03-09 10:45:46 +0100

    try cors

--- api/src/Routes/mail.Routes.ts
+++ api/src/Routes/mail.Routes.ts
@@ -1,11 +1,11 @@
 const express = require('express');
-import { corsMiddleware } from '../middleware/cors.middleware';
+// import { corsMiddleware } from '../middleware/cors.middleware';
 import { 
     sendMail
  } from "../Controller/emailService.controller";
 
  const router = express.Router();
 
-router.post('/send-email',corsMiddleware, sendMail);
+router.post('/send-email', sendMail);
 
 export default router;
