4afd75752cc9c2e3be9ffb49fd25640f5d7f742c
killian-lecorf <killianlecorf@outlook.fr>
2024-06-15 04:21:14 +0200

    add creation components pages

---
+++ lecerfdesign/src/Components/UI-kit/VideoCreationComponent/VideoCreationComponents.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VideoCreationComponents.scss';
+
+const VideoCreationComponents = () => {
+    return (
+        <div className='VideoCreationComponents'>
+            <p>res</p>
+        </div>
+    );
+};
+
+export default VideoCreationComponents;
---
+++ lecerfdesign/src/Components/VideoCreations/VideoCreations.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./VideoCreations.scss"
+import VideoCreationComponents from '../UI-kit/VideoCreationComponent/VideoCreationComponents';
+
+const VideoCreations = () => {
+    return (
+        <div className='VideoCreation'>
+            <VideoCreationComponents />
+        </div>
+    );
+};
+
+export default VideoCreations;
