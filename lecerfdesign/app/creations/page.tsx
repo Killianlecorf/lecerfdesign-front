ed185f4178b61edb0179fdc8366847feef21285c
killian-lecorf <Klecorf@normandiewebschool.fr>
2024-08-30 12:11:32 +0200

    Fix: reduce title in creation page

--- lecerfdesign/app/creations/page.tsx
+++ lecerfdesign/app/creations/page.tsx
@@ -2,8 +2,6 @@ import Footer from '@/Components/Footer/Footer';
 import Header from '@/Components/Header/Header';
 import React from 'react';
 import CreationPageVideo from "@/Components/CreationPageVideo/CreationPageVideo";
-// import Image from 'next/image';
-// import banniere from "../../public/Assets/Img/bannierecreation.svg";
 import CreationBismoulien from '@/Components/CreationBismoulien/CreationBismoulien';
 import CardImage from '@/Components/CardImage/CardImage';
 
